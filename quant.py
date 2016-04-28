0d9f1845831c6cccca85e8a2b67f475c9a01a44e
inso <insomniak.fr@gmaiL.com>
2016-04-28 18:18:27 +0200

    Fix views

--- quant.py
+++ quant.py
@@ -4,9 +4,9 @@ import matplotlib.colors as colors
 import matplotlib.animation as animation
 
 growth = 0.1
-members = 5
-mass = 1000
-du = mass/members
+members = 10
+mass = 0
+du = 100
 account_sup = 0.9*mass
 account_less = 0.01*mass
 
@@ -21,6 +21,8 @@ less = [[], []]
 dividend = []
 col = []
 
+start = 30
+
 def data_gen(t=0):
     global growth
     global mass
@@ -28,21 +30,21 @@ def data_gen(t=0):
     global account_sup
     global account_less
     cnt = 0
-    while cnt < 70:
+    while cnt < 80:
         cnt += 1
         t += 1
-        if t == 30:
+        if t == start:
             account_sup = 0.9 * mass
             account_less = 0.01 * mass
-        du = du*1.1
         mass = mass + members * du
         account_sup = account_sup + du
         account_less = account_less + du
+        du = max(du, growth*mass/members)
         yield ((t, mass), (t, du*members), (t, account_less), (t, account_sup))
 
 
 def init():
-    ax.set_xlim(30, 71)
+    ax.set_xlim(0, 81-start)
     ax.set_ylim(0, 1000)
     color_iter = iter(colors.cnames.items())
     next(color_iter)
@@ -60,27 +62,33 @@ def run(data):
     global mass
     # update the data
     m, d, acc_less, acc_sup = data
-    quant[0].append(m[0])
-    quant[1].append(m[1])
-    sup[0].append(acc_sup[0])
-    sup[1].append(acc_sup[1])
-    less[0].append(acc_less[0])
-    less[1].append(acc_less[1])
+    if m[0] > start - 5:
+        quant[0].append(m[0]-start)
+        quant[1].append(m[1])
+        sup[0].append(acc_sup[0]-start)
+        sup[1].append(acc_sup[1])
+        less[0].append(acc_less[0]-start)
+        less[1].append(acc_less[1])
 
-    for i in range(0, 10):
-        dividend[i][0].append(d[0])
-        dividend[i][1].append(d[1]*(i+1))
+        for i in range(0, 10):
+            dividend[i][0].append(d[0]-start)
+            dividend[i][1].append(m[1]*(i+1)/10)
 
-    ax.set_ylim(0, mass)
-    ax.figure.canvas.draw()
-    line_mass.set_data(quant[0], quant[1])
-    line_account_sup.set_data(sup[0], sup[1])
-    line_account_less.set_data(less[0], less[1])
-    for i in range(0, 10):
-        if i > 0:
-            ax.fill_between(dividend[i][0], dividend[i-1][1], dividend[i][1], color=col[i], alpha=0.1)
-        else:
-            ax.fill_between(dividend[i][0], 0, dividend[i][1], color=col[i], alpha=0.1)
+        ax.set_ylim(0, mass)
+        ax.figure.canvas.draw()
+        line_mass.set_data(quant[0], quant[1])
+        line_account_sup.set_data(sup[0], sup[1])
+        line_account_less.set_data(less[0], less[1])
+        for i in range(0, 10):
+            if i % 2 == 0:
+                col = 'powderblue'
+            else:
+                col = 'paleturquoise'
+            if i > 0:
+                ax.fill_between(dividend[i][0], dividend[i-1][1], dividend[i][1], color=col, alpha=0.1)
+            else:
+                ax.fill_between(dividend[i][0], 0, dividend[i][1], color=col, alpha=0.1)
+
     return line_account_less, line_account_sup, line_mass
 
 ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval=150,
@@ -89,6 +97,8 @@ ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval=150,
 
 # Set up formatting for the movie files
 Writer = animation.writers['ffmpeg']
-writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+writer = Writer(fps=10, metadata=dict(artist='Me'), bitrate=1800)
 
 ani.save('quant.mp4', writer=writer)
+
+#plt.show()
--- rel.py
+++ rel.py
@@ -4,9 +4,9 @@ import matplotlib.colors as colors
 import matplotlib.animation as animation
 
 growth = 0.1
-members = 5
-mass = 1000
-du = mass/members
+members = 10
+mass = 0
+du = 100
 account_sup = 0.9*mass
 account_less = 0.01*mass
 
@@ -20,6 +20,7 @@ sup = [[], []]
 less = [[], []]
 dividend = []
 col = []
+start = 30
 
 def data_gen(t=0):
     global growth
@@ -28,23 +29,23 @@ def data_gen(t=0):
     global account_sup
     global account_less
     cnt = 0
-    while cnt < 70:
+    while cnt < 80:
         cnt += 1
         t += 1
 
-        if t == 30:
+        if t == start:
             account_sup = 0.9 * mass
             account_less = 0.01 * mass
-        du = du*1.1
         mass = mass + members * du
         account_sup = account_sup + du
         account_less = account_less + du
-        yield ((t, mass/du), (t, members), (t, account_less/du), (t, account_sup/du))
+        du = max(du, growth*mass/members)
+        yield ((t, mass/du), (t, 1), (t, account_less/(du*1.1)), (t, account_sup/(du*1.1)))
 
 
 def init():
-    ax.set_xlim(30, 71)
-    ax.set_ylim(0, 1000)
+    ax.set_xlim(0, 81-start)
+    ax.set_ylim(0, 130)
     color_iter = iter(colors.cnames.items())
     next(color_iter)
     for i in range(0, 10):
@@ -61,27 +62,32 @@ def run(data):
     global mass
     # update the data
     m, d, acc_less, acc_sup = data
-    quant[0].append(m[0])
-    quant[1].append(m[1])
-    sup[0].append(acc_sup[0])
-    sup[1].append(acc_sup[1])
-    less[0].append(acc_less[0])
-    less[1].append(acc_less[1])
+    if m[0] > start - 5:
+        quant[0].append(m[0]-start)
+        quant[1].append(m[1])
+        sup[0].append(acc_sup[0]-start)
+        sup[1].append(acc_sup[1])
+        less[0].append(acc_less[0]-start)
+        less[1].append(acc_less[1])
 
-    for i in range(0, 10):
-        dividend[i][0].append(d[0])
-        dividend[i][1].append(d[1]*(i+1))
+        for i in range(0, 10):
+            dividend[i][0].append(d[0]-start)
+            dividend[i][1].append(m[1]*(i+1)/10)
 
-    ax.set_ylim(0, mass*1.2/du)
-    ax.figure.canvas.draw()
-    line_mass.set_data(quant[0], quant[1])
-    line_account_sup.set_data(sup[0], sup[1])
-    line_account_less.set_data(less[0], less[1])
-    for i in range(0, 10):
-        if i > 0:
-            ax.fill_between(dividend[i][0], dividend[i-1][1], dividend[i][1], color=col[i], alpha=0.1)
-        else:
-            ax.fill_between(dividend[i][0], 0, dividend[i][1], color=col[i], alpha=0.1)
+        ax.figure.canvas.draw()
+        line_mass.set_data(quant[0], quant[1])
+        line_account_sup.set_data(sup[0], sup[1])
+        line_account_less.set_data(less[0], less[1])
+        for i in range(0, 10):
+            if i % 2 == 0:
+                col = 'powderblue'
+            else:
+                col = 'paleturquoise'
+            if i > 0:
+                ax.fill_between(dividend[i][0], dividend[i-1][1], dividend[i][1], color=col, alpha=0.1)
+            else:
+                ax.fill_between(dividend[i][0], 0, dividend[i][1], color=col, alpha=0.1)
+
     return line_account_less, line_account_sup, line_mass
 
 ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval=150,
@@ -90,7 +96,7 @@ ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval=150,
 
 # Set up formatting for the movie files
 Writer = animation.writers['ffmpeg']
-writer = Writer(fps=3, metadata=dict(artist='Me'), bitrate=1800)
+writer = Writer(fps=10, metadata=dict(artist='Me'), bitrate=1800)
 #
 ani.save('rel.mp4', writer=writer)
 
